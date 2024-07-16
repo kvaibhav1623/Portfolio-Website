0bed2bc46db0c59ceff27cdc6a06998e0cefb47e
Vaibhav Kumar <kvaibhav082@gmail.com>
2024-07-16 12:22:08 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Vaibhav &nbsp;
-            <span className="sm:block hidden">| Aspiring SDE</span>
+            <span className="sm:block hidden">| Software Engineer</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
